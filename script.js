23554a94e817d7805656ba83603c461f006da736
Ajaytiwari0811 <ajay@depasserinfotech.in>
2023-08-10 14:41:17 +0530

    mobile animation

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-$(document).ready(function() {
-    var $magic = $(".magic"),
-        magicWHalf = $magic.width() / 2;
-    $(document).on("mousemove", function(e) {
-      $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
-    });
+$(document).ready(function () {
+  var $magic = $(".magic"),
+    magicWHalf = $magic.width() / 2;
+  $(document).on("mousemove", function (e) {
+    $magic.css({ "left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf });
   });
+});
+
+
+console.log(screen.width)
+if (screen.width <= 1024) {
+  var tl = gsap.timeline()
+    .to('.magic', {
+      left: "random(-100, 100,5)", //chooses a random number between -20 and 20 for each target, rounding to the closest 5!
+      top: "random(-100, 400,5)",
+      duration: 2,
+      ease: "power1.inOut",
+      repeat: -1,
+      repeatRefresh: true // gets a new random leftand y value on each repeat
+    })
+}
