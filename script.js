f86297ab5f0bd4882b9307d22d3c5320c0ce1c8c
Ajaytiwari0811 <ajay@depasserinfotech.in>
2023-08-11 17:28:32 +0530

    debugging on safari

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-  var $magic = $(".magic"),
-    magicWHalf = $magic.width() / 2;
+  var magic = $(".magic"),
+    magicWHalf = magic.width() / 2;
   $(document).on("mousemove", function (e) {
-    $magic.css({ "left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf });
+    magic.css({ "left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf });
   });
 });
 
